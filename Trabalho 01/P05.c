5384f0c10d8f2ad8554dd9de26209d0d81842f80
Pedro Henrique Holzmeier Schaefer <pedro.schaefer@estudante.uffs.edu.br>
2025-08-14 09:36:13 -0300

    p05

--- Trabalho 01/P05.c
+++ Trabalho 01/P05.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 int main(){
+    int n1;
+    int n2;
+    int resultado = 0;
+    int contador;
 
+    printf("Multiplicando: ");
+    scanf("%d", &n1);
 
+    printf("Multiplicador: ");
+    scanf("%d", &n2);
 
+    contador = n2;
 
+    if (n2 >= 0){
 
-    
+        while (contador > 0)
+        {
+            resultado += n1;
+            contador--;
+        }
+    }
+
+    else{
+        while (contador < 0)
+        {
+            resultado -= n1;
+            contador++;
+
+        }
+    }
+
+    printf("%d * %d = %d\n",n1, n2, resultado);
     return 0;
 }
