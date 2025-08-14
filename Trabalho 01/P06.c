a99068b4e60e6eefba69cd165b4af75e9514ee62
Pedro Henrique Holzmeier Schaefer <pedro.schaefer@estudante.uffs.edu.br>
2025-08-14 13:33:21 -0300

    p06

---
+++ Trabalho 01/P06.c
@@ -0,0 +1,22 @@
+//imprima a soma de todos os números de 1 até o valor informado.
+
+#include <stdio.h>
+int main(){
+    int num;
+    int contador;
+    int soma = 0;
+    printf("Digite o numero: ");
+    scanf("%d", &num);
+
+
+    contador = num;
+    while (contador > 0)
+    {
+        soma += contador;
+        contador--;
+    }
+    printf("Resultado: %d\n", soma);
+    return 0;
+
+
+}
