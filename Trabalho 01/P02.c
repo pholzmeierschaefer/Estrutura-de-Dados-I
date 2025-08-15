e1445999ff928c4c93c4a7738fa2a314a8f593ca
Pedro Henrique Holzmeier Schaefer <pedro.schaefer@estudante.uffs.edu.br>
2025-08-15 18:01:32 -0300

    p02

--- Trabalho 01/P02.c
+++ Trabalho 01/P02.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     float preco;
     printf("Preco do produto: ");
-    scanf("%f", &preco);   
+    scanf("%f", &preco);
     int forma_pag;
     printf("Forma de pagamento \n1 - A vista \n2 - A prazo\n");
     scanf("%d", &forma_pag);
 
-    if (forma_pag == 1) {
+    if (forma_pag == 1)
+    {
         preco = preco - (preco * 0.05);
         printf("Preco a vista: %.2f\n", preco);
     }
-    
-    else if (forma_pag == 2){
+
+    else if (forma_pag == 2)
+    {
         preco = preco + (preco * 0.10);
         printf("Preco a prazo: %.2f\n", preco);
     }
