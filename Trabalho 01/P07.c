f952b642df5adc31f56c82076c5b76222e43ff3a
Pedro Henrique Holzmeier Schaefer <pedro.schaefer@estudante.uffs.edu.br>
2025-08-15 14:03:25 -0300

    p07

---
+++ Trabalho 01/P07.c
@@ -0,0 +1,36 @@
+// sequência de n números (um número menor ou igual à 0 finaliza a sequência),
+// apresentar o percentual de números informados que são maiores ou igual a 10 e menores ou igual a 20.
+
+#include <stdio.h>
+int main()
+{
+    int respondem = 0;
+    int total = 0;
+    int numero;
+
+    printf("Digite um numero (menor ou igual 0 termina a sequencia): ");
+    scanf("%d", &numero);
+
+    while (numero > 0)
+    {
+        total++;
+
+        if (numero >= 10 && numero <= 20)
+        {
+            respondem++;
+        }
+        printf("Digite um numero (menor ou igual 0 termina a sequencia): ");
+        scanf("%d", &numero);
+    }
+
+    if (total > 0)
+    {
+        float percentual = (respondem * 100.0) / total;
+        printf("Percentual de numeros entre 10 e 20: %.2f%%\n", percentual);
+    }
+    else
+    {
+        printf("Nenhum numero informado.\n");
+    }
+    return 0;
+}
